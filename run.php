e98e00d9348f2f5a064bfd9fa6b1d611268dd32e
andy <andy@chegod.ru>
2014-02-19 00:11:21 +0400

    run.php moved to more common one with input arguments

--- run.php
+++ run.php
@@ -8,11 +8,27 @@
 
 require_once("Fetcher.php");
 
-$url = "http://public-yum.oracle.com/repo/OracleLinux/OL6/latest/x86_64/";
-$outDir = "/Users/andy/Repos/oracle-6/base/";
-//$url = "http://public-yum.oracle.com/repo/OracleLinux/OL6/UEKR3/latest/x86_64/";
-//$outDir = "/Users/andy/Repos/oracle-6/uekr3/";
 
-$fetcher = new Fetcher($url, $outDir);
+$repoUrl = $argv[1];
+$outDir = $argv[2];
+
+try
+{
+	if (empty($repoUrl))
+		throw new Exception("Repository URL is empty");
+
+	if (empty($outDir))
+		throw new Exception("Output directory is empty");
+
+	if (!is_dir($outDir))
+		throw new Exception("'$outDir' is not a directory");
+}
+catch (Exception $e)
+{
+	echo "Usage: {$argv[0]} <repo-url> <output-dir>\n\n";
+	echo $e->getMessage();
+	echo "\n";
+}
+
+$fetcher = new Fetcher($repoUrl, $outDir);
 $fetcher->run();
-
